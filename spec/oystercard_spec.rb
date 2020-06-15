bd50d158b439a6ccf73ab8a003c15427f3b4bb64
Iryna Audzei <avirene@live.com>
2020-06-15 16:15:51 +0000

    Added the top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,25 @@
 require 'oystercard'
 describe Oystercard do
-    describe 'initialize' do
-        it 'can create an instance of oystercard' do
-            oystercard = Oystercard.new
-            expect(oystercard).to be_an_instance_of(Oystercard)
-        end
+  let(:oystercard) { Oystercard.new }
+  
+  describe 'initialize' do
+    it 'can create an instance of oystercard' do
+      expect(oystercard).to be_an_instance_of(Oystercard)
     end
-    describe '#balance' do
-      it 'checks balance' do
-        oystercard = Oystercard.new
-        expect(oystercard.balance).to eq(0)
-      end
+  end
+    
+  describe '#balance' do
+    it 'checks balance' do
+      expect(oystercard.balance).to eq(0)
     end
+  end
+    
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
+    end
+  end
+    
 end
