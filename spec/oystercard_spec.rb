206752dedd457ca8de1b38c69427a7cc06fc1d26
Iryna Audzei <avirene@live.com>
2020-06-15 15:00:06 +0000

    Added the attribure reader balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require 'oystercard'
 describe Oystercard do
-    
+    describe 'initialize' do
+        it 'can create an instance of oystercard' do
+            oystercard = Oystercard.new
+            expect(oystercard).to be_an_instance_of(Oystercard)
+        end
+    end
+    describe '#balance' do
+      it 'checks balance' do
+        oystercard = Oystercard.new
+        expect(oystercard.balance).to eq(0)
+      end
+    end
 end
